bd08ebb5726815d1d257d802c793455005ee1cc5
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-04-16 12:34:23 -0400

    feat: initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * This script copies labels from one repo to the next
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ * @param {object} options
+ * @param {string} options.template this is the repo you want the labels to be copied from
+ */
+export async function script(octokit, repository, { template }) {}
