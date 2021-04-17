8f66a337463654a6c22c1284dd029c9c3ef528dc
Brian Douglas <bdougie@users.noreply.github.com>
2021-04-17 17:48:48 -0400

    feat: Loop to check all labels (#3)
    * the script works, not iterate
    
    * script n+1 through all labels
    
    * adds note about bypass

--- script.js
+++ script.js
@@ -8,4 +8,53 @@
  * @param {object} options
  * @param {string} options.template this is the repo you want the labels to be copied from
  */
-export async function script(octokit, repository, { template }) {}
+export async function script(octokit, repository, { template }) {
+  if (!template) {
+    throw new Error(`--template is required`);
+  }
+
+  octokit.log.debug(
+    "Load branch protection settings from template repository %s",
+    template
+  );
+
+  const [templateOwner, templateRepo] = template.split("/");
+  const [repoOwner, repoName] = repository.full_name.split("/");
+
+  const labels = await octokit.request('GET /repos/{owner}/{repo}/labels', {
+    owner: templateOwner,
+    repo: templateRepo
+  })
+  try {
+    for (let i = 0; i < labels.data.length; i++) {
+      const {name, description, color} = labels.data[i];
+
+      const exists = await octokit.request('GET /repos/{owner}/{repo}/labels/{name}', {
+        owner: repoOwner,
+        repo: repoName,
+        name,
+      }).then(() => true, () => false)
+
+      octokit.log.info(`${name} label exists: ${exists}`)
+      
+      if (!exists) {
+        const label = await octokit.request('POST /repos/{owner}/{repo}/labels', {
+          owner: repoOwner,
+          repo: repoName,
+          name,
+          description,
+          color
+        })
+
+        octokit.log.info(`${name} updated`)
+      }
+    }
+  } catch(e) {
+    octokit.log.error(e)
+  }
+
+  // console.log(response)
+}
+
+
+
